7318b34273e817696f42715b6317527e2a0ab02a
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2021-11-28 00:21:02 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,33 @@
+// importing dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+// making an express app instance
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8000, () => {
+  console.log("Backend is running!");
+});
