e776cca1a101ce57213b53f42067748ce0d93644
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2021-11-28 01:52:08 +0530

    added UPDATE and DELETE user APIs

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,47 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("Hey it's user's route");
+// update a user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can only update your accont");
+  }
 });
+
+// delete a user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can only delete your accont");
+  }
+});
+
+// get a user
+// follow a user
+// unfollow a user
 
 module.exports = router;
